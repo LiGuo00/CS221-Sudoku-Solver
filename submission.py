57801b7d1880c31295a72036e80e2ab936b73384
maoxianjiang <44913385+maoxianjiang@users.noreply.github.com>
2018-11-15 11:07:46 -0800

    Create submission.py

---
+++ submission.py
@@ -0,0 +1,394 @@
+import collections, copy
+
+def create_suduko(n = 4):
+    '''
+    Create suduku variables and constraints without the input
+    :param n:
+    :return: csp
+    '''
+    csp = CSP()
+    def getnum(i,j):
+        return (i - 1) / int(n ** 0.5) * int(n ** 0.5) + (j - 1) / int(n ** 0.5) + 1
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            csp.add_variable((i, j, getnum(i, j)), range(1, n+1)) # i:row, j:column, getnum(i, j): position of (i,j) in sudoku
+            #edit here if we have input
+    for val1 in csp.variables:
+        for val2 in csp.variables:
+            if val1 == val2:
+                continue
+            if val1[0] == val2[0]:
+                csp.add_binary_factor(val1, val2, lambda x, y: x != y)
+            if val1[1] == val2[1]:
+                csp.add_binary_factor(val1, val2, lambda x, y: x != y)
+            if val1[2] == val2[2]:
+                csp.add_binary_factor(val1, val2, lambda x, y: x != y)
+    return csp
+
+
+# A backtracking algorithm that solves weighted CSP.
+# Usage:
+#   search = BacktrackingSearch()
+#   search.solve(csp)
+class BacktrackingSearch():
+
+    def reset_results(self):
+        """
+        This function resets the statistics of the different aspects of the
+        CSP solver. We will be using the values here for grading, so please
+        do not make any modification to these variables.
+        """
+        # Keep track of the best assignment and weight found.
+        self.optimalAssignment = {}
+        self.optimalWeight = 0
+
+        # Keep track of the number of optimal assignments and assignments. These
+        # two values should be identical when the CSP is unweighted or only has binary
+        # weights.
+        self.numOptimalAssignments = 0
+        self.numAssignments = 0
+
+        # Keep track of the number of times backtrack() gets called.
+        self.numOperations = 0
+
+        # Keep track of the number of operations to get to the very first successful
+        # assignment (doesn't have to be optimal).
+        self.firstAssignmentNumOperations = 0
+
+        # List of all solutions found.
+        self.allAssignments = []
+
+    def print_stats(self):
+        """
+        Prints a message summarizing the outcome of the solver.
+        """
+        if self.optimalAssignment:
+            print "Found %d optimal assignments with weight %f in %d operations" % \
+                (self.numOptimalAssignments, self.optimalWeight, self.numOperations)
+            print "First assignment took %d operations" % self.firstAssignmentNumOperations
+        else:
+            print "No solution was found."
+
+    def get_delta_weight(self, assignment, var, val):
+        """
+        Given a CSP, a partial assignment, and a proposed new value for a variable,
+        return the change of weights after assigning the variable with the proposed
+        value.
+
+        @param assignment: A dictionary of current assignment. Unassigned variables
+            do not have entries, while an assigned variable has the assigned value
+            as value in dictionary. e.g. if the domain of the variable A is [5,6],
+            and 6 was assigned to it, then assignment[A] == 6.
+        @param var: name of an unassigned variable.
+        @param val: the proposed value.
+
+        @return w: Change in weights as a result of the proposed assignment. This
+            will be used as a multiplier on the current weight.
+        """
+        assert var not in assignment
+        w = 1.0
+        if self.csp.unaryFactors[var]:
+            w *= self.csp.unaryFactors[var][val]
+            if w == 0: return w
+        for var2, factor in self.csp.binaryFactors[var].iteritems():
+            if var2 not in assignment: continue  # Not assigned yet
+            w *= factor[val][assignment[var2]]
+            if w == 0: return w
+        return w
+
+    def solve(self, csp, lookahead = False, mcv = False, ac3 = False):
+        """
+        Solves the given weighted CSP using heuristics as specified in the
+        parameter. Note that unlike a typical unweighted CSP where the search
+        terminates when one solution is found, we want this function to find
+        all possible assignments. The results are stored in the variables
+        described in reset_result().
+
+        @param csp: A weighted CSP.
+        @param mcv: When enabled, Most Constrained Variable heuristics is used.
+        @param ac3: When enabled, AC-3 will be used after each assignment of an
+            variable is made.
+        """
+        # Lookahead
+        self.lookahead = lookahead
+        # CSP to be solved.
+        self.csp = csp
+
+        # Set the search heuristics requested asked.
+        self.mcv = mcv
+        self.ac3 = ac3
+
+        # Reset solutions from previous search.
+        self.reset_results()
+
+        # The dictionary of domains of every variable in the CSP.
+        self.domains = {var: list(self.csp.values[var]) for var in self.csp.variables}
+
+        # Perform backtracking search.
+        self.backtrack({}, 0, 1)
+        # Print summary of solutions.
+        self.print_stats()
+
+    def backtrack(self, assignment, numAssigned, weight):
+        """
+        Perform the back-tracking algorithms to find all possible solutions to
+        the CSP.
+
+        @param assignment: A dictionary of current assignment. Unassigned variables
+            do not have entries, while an assigned variable has the assigned value
+            as value in dictionary. e.g. if the domain of the variable A is [5,6],
+            and 6 was assigned to it, then assignment[A] == 6.
+        @param numAssigned: Number of currently assigned variables
+        @param weight: The weight of the current partial assignment.
+        """
+        self.numOperations += 1
+        assert weight > 0
+        if numAssigned == self.csp.numVars:
+            # A satisfiable solution have been found. Update the statistics.
+            self.numAssignments += 1
+            newAssignment = {}
+            for var in self.csp.variables:
+                newAssignment[var] = assignment[var]
+            self.allAssignments.append(newAssignment)
+
+            if len(self.optimalAssignment) == 0 or weight >= self.optimalWeight:
+                if weight == self.optimalWeight:
+                    self.numOptimalAssignments += 1
+                else:
+                    self.numOptimalAssignments = 1
+                self.optimalWeight = weight
+
+                self.optimalAssignment = newAssignment
+                if self.firstAssignmentNumOperations == 0:
+                    self.firstAssignmentNumOperations = self.numOperations
+            return
+
+        # Select the next variable to be assigned.
+        var = self.get_unassigned_variable(assignment)
+        # Get an ordering of the values.
+        ordered_values = self.domains[var]
+
+        # Continue the backtracking recursion using |var| and |ordered_values|.
+        if not self.ac3:
+            # When arc consistency check is not enabled.
+            for val in ordered_values:
+                if not self.lookahead:
+                    deltaWeight = self.get_delta_weight(assignment, var, val)
+                    assignment[var] = val
+                    self.backtrack(assignment, numAssigned + 1, weight * deltaWeight)
+                    del assignment[var]
+                else:
+                    deltaWeight = self.get_delta_weight(assignment, var, val)
+                    if deltaWeight > 0:
+                        assignment[var] = val
+                        self.backtrack(assignment, numAssigned + 1, weight * deltaWeight)
+                        del assignment[var]
+        else:
+            # Arc consistency check is enabled.
+            # Problem 1c: skeleton code for AC-3
+            # You need to implement arc_consistency_check().
+            for val in ordered_values:
+                deltaWeight = self.get_delta_weight(assignment, var, val)
+                if deltaWeight > 0:
+                    assignment[var] = val
+                    # create a deep copy of domains as we are going to look
+                    # ahead and change domain values
+                    localCopy = copy.deepcopy(self.domains)
+                    # fix value for the selected variable so that hopefully we
+                    # can eliminate values for other variables
+                    self.domains[var] = [val]
+
+                    # enforce arc consistency
+                    self.arc_consistency_check(var)
+
+                    self.backtrack(assignment, numAssigned + 1, weight * deltaWeight)
+                    # restore the previous domains
+                    self.domains = localCopy
+                    del assignment[var]
+
+    def get_unassigned_variable(self, assignment):
+        """
+        Given a partial assignment, return a currently unassigned variable.
+
+        @param assignment: A dictionary of current assignment. This is the same as
+            what you've seen so far.
+
+        @return var: a currently unassigned variable.
+        """
+
+        if not self.mcv:
+            # Select a variable without any heuristics.
+            for var in self.csp.variables:
+                if var not in assignment: return var
+        else:
+            # Heuristic: most constrained variable (MCV)
+            minVar = None
+            minNum = float('inf')
+            for var in self.csp.variables:
+                if var not in assignment:
+                    domain = self.domains[var]
+                    num = 0
+                    for val in domain:
+                        if self.get_delta_weight(assignment, var, val)>0:
+                            num += 1
+                    if num < minNum:
+                        minNum = num;
+                        minVar = var
+            return minVar
+            # END_YOUR_CODE
+    def arc_consistency_check(self, var):
+        """
+                Perform the AC-3 algorithm. The goal is to reduce the size of the
+                domain values for the unassigned variables based on arc consistency.
+
+                @param var: The variable whose value has just been set.
+        """
+        def domainChanged(var,var2):
+            changed = False
+            varDomain = list(self.domains[var])
+            var2Domain = list(self.domains[var2])
+            for val2 in var2Domain:
+                con = False
+                for val1 in varDomain:
+                    if self.csp.binaryFactors[var][var2] != None:
+                        if self.csp.binaryFactors[var][var2][val1][val2] != 0:
+                            con = True
+                    if self.csp.unaryFactors[var2] !=None:
+                        if self.csp.unaryFactors[var2][val2] != 0:
+                            con = True
+                if con == False:
+                    if val2 in self.domains[var2]:
+                        self.domains[var2].remove(val2)
+                        changed = True
+            return changed
+        qu = collections.deque([var])
+        while qu:
+            xvar = qu.popleft()
+            for var2 in self.csp.get_neighbor_vars(xvar):
+                if domainChanged(xvar,var2):
+                    qu.append(var2)
+
+
+# General code for representing a weighted CSP (Constraint Satisfaction Problem).
+# All variables are being referenced by their index instead of their original
+# names.
+class CSP:
+    def __init__(self):
+        # Total number of variables in the CSP.
+        self.numVars = 0
+
+        # The list of variable names in the same order as they are added. A
+        # variable name can be any hashable objects, for example: int, str,
+        # or any tuple with hashtable objects.
+        self.variables = []
+
+        # Each key K in this dictionary is a variable name.
+        # values[K] is the list of domain values that variable K can take on.
+        self.values = {}
+
+        # Each entry is a unary factor table for the corresponding variable.
+        # The factor table corresponds to the weight distribution of a variable
+        # for all added unary factor functions. If there's no unary function for
+        # a variable K, there will be no entry for K in unaryFactors.
+        # E.g. if B \in ['a', 'b'] is a variable, and we added two
+        # unary factor functions f1, f2 for B,
+        # then unaryFactors[B]['a'] == f1('a') * f2('a')
+        self.unaryFactors = {}
+
+        # Each entry is a dictionary keyed by the name of the other variable
+        # involved. The value is a binary factor table, where each table
+        # stores the factor value for all possible combinations of
+        # the domains of the two variables for all added binary factor
+        # functions. The table is represented as a dictionary of dictionary.
+        #
+        # As an example, if we only have two variables
+        # A \in ['b', 'c'],  B \in ['a', 'b']
+        # and we've added two binary functions f1(A,B) and f2(A,B) to the CSP,
+        # then binaryFactors[A][B]['b']['a'] == f1('b','a') * f2('b','a').
+        # binaryFactors[A][A] should return a key error since a variable
+        # shouldn't have a binary factor table with itself.
+
+        self.binaryFactors = {}
+
+    def add_variable(self, var, domain):
+        """
+        Add a new variable to the CSP.
+        """
+        if var in self.variables:
+            raise Exception("Variable name already exists: %s" % str(var))
+
+        self.numVars += 1
+        self.variables.append(var)
+        self.values[var] = domain
+        self.unaryFactors[var] = None
+        self.binaryFactors[var] = dict()
+
+
+    def get_neighbor_vars(self, var):
+        """
+        Returns a list of variables which are neighbors of |var|.
+        """
+        return self.binaryFactors[var].keys()
+
+    def add_unary_factor(self, var, factorFunc):
+        """
+        Add a unary factor function for a variable. Its factor
+        value across the domain will be *merged* with any previously added
+        unary factor functions through elementwise multiplication.
+
+        How to get unary factor value given a variable |var| and
+        value |val|?
+        => csp.unaryFactors[var][val]
+        """
+        factor = {val:float(factorFunc(val)) for val in self.values[var]}
+        if self.unaryFactors[var] is not None:
+            assert len(self.unaryFactors[var]) == len(factor)
+            self.unaryFactors[var] = {val:self.unaryFactors[var][val] * \
+                factor[val] for val in factor}
+        else:
+            self.unaryFactors[var] = factor
+
+    def add_binary_factor(self, var1, var2, factor_func):
+        """
+        Takes two variable names and a binary factor function
+        |factorFunc|, add to binaryFactors. If the two variables already
+        had binaryFactors added earlier, they will be *merged* through element
+        wise multiplication.
+
+        How to get binary factor value given a variable |var1| with value |val1|
+        and variable |var2| with value |val2|?
+        => csp.binaryFactors[var1][var2][val1][val2]
+        """
+        # never shall a binary factor be added over a single variable
+        try:
+            assert var1 != var2
+        except:
+            print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+            print '!! Tip:                                                                       !!'
+            print '!! You are adding a binary factor over a same variable...                  !!'
+            print '!! Please check your code and avoid doing this.                               !!'
+            print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+            raise
+
+        self.update_binary_factor_table(var1, var2,
+            {val1: {val2: float(factor_func(val1, val2)) \
+                for val2 in self.values[var2]} for val1 in self.values[var1]})
+        self.update_binary_factor_table(var2, var1, \
+            {val2: {val1: float(factor_func(val1, val2)) \
+                for val1 in self.values[var1]} for val2 in self.values[var2]})
+
+    def update_binary_factor_table(self, var1, var2, table):
+        """
+        Private method you can skip for 0c, might be useful for 1c though.
+        Update the binary factor table for binaryFactors[var1][var2].
+        If it exists, element-wise multiplications will be performed to merge
+        them together.
+        """
+        if var2 not in self.binaryFactors[var1]:
+            self.binaryFactors[var1][var2] = table
+        else:
+            currentTable = self.binaryFactors[var1][var2]
+            for i in table:
+                for j in table[i]:
+                    assert i in currentTable and j in currentTable[i]
+                    currentTable[i][j] *= table[i][j]
