f9593a8c7b0454366503c231fa49bbdfb3a908c5
maoxianjiang <44913385+maoxianjiang@users.noreply.github.com>
2018-11-15 11:07:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import submission
+
 print "hello world"
+csp = submission.create_suduko(4)
+alg = submission.BacktrackingSearch()
+alg.solve(csp, True, True, True)
+print csp.variables
+print 'One of the optimal assignments:',  alg.optimalAssignment
+
